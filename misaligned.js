98f81a34161d80f4ee69d13c6106b63e186fe2f3
AJEET KUMAR <cajeet@hotmail.com>
2022-05-31 21:07:42 +0530

    Add files via upload

--- misaligned.js
+++ misaligned.js
@@ -1,16 +1,31 @@
-const {expect} = require('chai')
+const { expect } = require('chai');
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
-    for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
-    }
-    return majorColors.length * minorColors.length;
+const majorColors = ['White', 'Red', 'Black', 'Yellow', 'Violet'];
+const minorColors = ['Blue', 'Orange', 'Green', 'Brown', 'Slate'];
+
+function print_color_map(
+	primaryColor,
+	secondaryColor,
+	printColor,
+	getColorsCount
+) {
+	printColor(primaryColor, secondaryColor);
+	let colorCount = getColorsCount(primaryColor, secondaryColor);
+	return colorCount;
 }
 
-result = print_color_map();
+function printColor(primaryColor, secondaryColor) {
+	for (let i = 0; i < majorColors.length; i++) {
+		for (let j = 0; j < minorColors.length; j++) {
+			console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
+		}
+	}
+}
+
+function getColorsCount(primaryColor, secondaryColor) {
+	return primaryColor.length * secondaryColor.length;
+}
+
+result = print_color_map(majorColors, minorColors, printColor, getColorsCount);
 expect(result).equals(25);
 console.log('All is well (maybe!)');
--- tshirts.js
+++ tshirts.js
@@ -1,16 +1,17 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 
 function size(cms) {
-    if (cms < 38) {
-        return 'S';
-    } else if (cms > 38 && cms < 42) {
-        return 'M';
-    } else {
-        return 'L';
-    }
+	if (cms < 38) {
+		return 'S';
+	} else if (cms > 38 && cms < 42) {
+		return 'M';
+	} else {
+		return 'L';
+	}
 }
 
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+expect(size(38)).to.Throw(Error);
 console.log('All is well (maybe!)');
