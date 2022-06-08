39e328f3b6970eb5c3307634f653efd1d47afc2d
AJEET KUMAR <cajeet@hotmail.com>
2022-06-08 10:34:50 +0530

    Update tshirts.js

--- tshirts.js
+++ tshirts.js
@@ -10,8 +10,8 @@ function size(cms) {
 	}
 }
 
-expect(size(37)).equals('M');
-expect(size(40)).equals('L');
-expect(size(43)).equals('S');
+expect(size(37)).equals('S');
+expect(size(40)).equals('M');
+expect(size(43)).equals('L');
 expect(size(38)).equals('S');
 console.log('All is well (maybe!)');
